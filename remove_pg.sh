aae38e29402b8d65f08748f56e49eff76e47d2b9
Selena Deckelmann <selenamarie@gmail.com>
2011-07-23 17:14:21 -0700

    Script to create a demo sync repl master/slave pair

--- remove_pg.sh
+++ remove_pg.sh
@@ -1,4 +1,7 @@
 export PATH=/opt/pg91beta2/bin:$PATH
 pg_ctl -D oscon_demo stop -m immediate
+pg_ctl -D oscon_demo_repl stop -m immediate
 
 rm -rf oscon_demo
+rm -rf oscon_demo_repl
+rm -rf /tmp/wal
