9715566037761e3b9e5e0ad9a0563e3b0f9c41ad
Selena Deckelmann <selenamarie@gmail.com>
2012-01-13 05:20:46 +1100

    also cleanup logfiles

--- remove_demo.sh
+++ remove_demo.sh
@@ -6,5 +6,7 @@ pg_ctl -D $DATA_DIR stop -m immediate
 pg_ctl -D ${DATA_DIR}_repl stop -m immediate
 
 rm -rf $DATA_DIR
+rm -rf $DATA_DIR.log
 rm -rf ${DATA_DIR}_repl
+rm -rf ${DATA_DIR}_repl.log
 rm -rf $WAL
