a8d971f0876ddf8ae3132eb110b942d534326c8e
Selena Deckelmann <selenamarie@gmail.com>
2012-12-28 14:27:45 -0800

    Adding config for auto-promotion

--- repmgr/setup.sh
+++ repmgr/setup.sh
@@ -11,17 +11,42 @@ cluster=${DEMO}
 node=1
 node_name=earth
 conninfo='host=localhost user=repmgr dbname=pgbench port=${PORT}'
+# How many seconds we wait for master response before declaring master failure
+master_response_timeout=60
+
+# How many time we try to reconnect to master before starting failover procedure
+reconnect_attempts=6
+reconnect_interval=10
+
+# Autofailover options
+failover=automatic
+priority=-1
+promote_command='repmgr standby promote -f ${REPMGRCONF}/repmgr.conf'
+follow_command='repmgr standby follow -f ${REPMGRCONF}/repmgr.conf -W'
 RMC
-) >> ${REPMGRCONF}/repmgr.conf
+) > ${REPMGRCONF}/repmgr.conf
 
 (
 cat <<-RMC2
-  cluster=${DEMO}
-  node=2
-  node_name=mars
-  conninfo='host=localhost user=repmgr dbname=pgbench port=${PORT2}'
+cluster=${DEMO}
+node=2
+node_name=mars
+conninfo='host=localhost user=repmgr dbname=pgbench port=${PORT2}'
+
+# How many seconds we wait for master response before declaring master failure
+master_response_timeout=60
+
+# How many time we try to reconnect to master before starting failover procedure
+reconnect_attempts=6
+reconnect_interval=10
+
+# Autofailover options
+failover=automatic
+priority=-1
+promote_command='repmgr standby promote -f ${REPMGRCONF}/repmgr2.conf'
+follow_command='repmgr standby follow -f ${REPMGRCONF}/repmgr2.conf -W'
 RMC2
-) >> ${REPMGRCONF}/repmgr2.conf
+) > ${REPMGRCONF}/repmgr2.conf
 
 /usr/lib/postgresql/9.2/bin/initdb ${DEMO}
 
@@ -38,7 +63,7 @@ cat <<-DEMO
 DEMO
 )  >> ${DEMO}/pg_hba.conf
 
-$PGCTL -D ${DEMO} start
+$PGCTL -D ${DEMO} start > ${DEMO}.log 2>&1
 
 sleep 3
 
@@ -59,9 +84,14 @@ port=$PORT2
 NEWPORT
 ) >> ${DEMO2}/postgresql.conf
 
-$PGCTL -D ${DEMO2} start
+$PGCTL -D ${DEMO2} start > ${DEMO2}.log 2>&1
 
 repmgr -f ${REPMGRCONF}/repmgr.conf --verbose master register 
-repmgrd -f ${REPMGRCONF}/repmgr.conf --verbose > ${REPLOG}/repmgr.log 2>&1 & 
+
+PGENGINE=/usr/lib/postgresql/9.2/bin PATH="$PATH:$PGENGINE" repmgrd -f ${REPMGRCONF}/repmgr.conf --verbose > ${REPLOG}/repmgr.log 2>&1 & 
+
+sleep 4
+
+PGENGINE=/usr/lib/postgresql/9.2/bin PATH="$PATH:$PGENGINE" repmgrd -f ${REPMGRCONF}/repmgr2.conf --verbose > ${REPLOG}/repmgr2.log 2>&1 & 
 
 #repmgr -f ${REPMGRCONF}/repmgr2.conf --verbose standby register
--- repmgr/teardown.sh
+++ repmgr/teardown.sh
@@ -4,6 +4,8 @@
 
 $PGCTL -D ${DEMO} stop -m fast
 rm -rf ${DEMO}
+rm ${DEMO}.log
 
 $PGCTL -D ${DEMO2} stop -m fast
 rm -rf ${DEMO2}
+rm ${DEMO2}.log
